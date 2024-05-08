62b417f2a03f428c1f3278bd19aa1167c4b34b28
akmaljon1946 <akmaljon.ahmedjanov@yandex.ru>
2024-05-08 20:29:17 +0500

    animat

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-const menuham = document.querySelector('.header__mobile-btn');
-const menu = document.querySelector('.header__menu');
-const closeBtn = document.querySelector('.header__menu-closed--button');
+const menuham = document.querySelector('.header__mobile-btn')
+const menu = document.querySelector('.header__menu')
+const closeBtn = document.querySelector('.header__menu-closed--button')
 // const curtain = document.querySelector('.curtain');
 
-menuham.addEventListener('click', (e) => {
-    menu.classList.add('header__menu--on');
-    // curtain.classList.add('curtain--on')
+menuham.addEventListener('click', e => {
+	menu.classList.add('header__menu--on')
+	menu.classList.add('animation-in')
+	// curtain.classList.add('curtain--on')
 })
 
 closeBtn.addEventListener('click', () => {
-    menu.classList.remove('header__menu--on')
-    // curtain.classList.remove('curtain--on')
+	menu.classList.remove('header__menu--on')
+	// curtain.classList.remove('curtain--on')
 })
